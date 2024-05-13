ff42447f62469a43b24c220579ed72e1be4b8999
sushantdigitalat <sushant.khairnar@digitalat.in>
2024-05-13 18:03:24 +0530

    Landing page

--- assets/js/main-min.js
+++ assets/js/main-min.js
@@ -1,40 +1,61 @@
-!(function (s) {
-  s(window).on("load", function () {
-    s("#preloader-active").delay(450).fadeOut("slow"),
-      s("body").delay(450).css({ overflow: "visible" });
+!(function (e) {
+  e(window).on("load", function () {
+    e("#preloader-active").delay(450).fadeOut("slow"),
+      e("body").delay(450).css({ overflow: "visible" });
   });
-  var e = s("ul#navigation");
-  e.length &&
-    e.slicknav({
+  var o = e("ul#navigation");
+  o.length &&
+    o.slicknav({
       prependTo: ".mobile-menu",
       closedSymbol: "+",
       openedSymbol: "-",
     }),
-    s(document).ready(function () {
-      var e = s(".hamburger"),
-        o = s(".overlay"),
-        l = !1;
-      e.click(function () {
-        1 == l
-          ? (o.hide(),
-            e.removeClass("is-open"),
-            e.addClass("is-closed"),
-            (l = !1))
-          : (o.show(),
-            e.removeClass("is-closed"),
-            e.addClass("is-open"),
-            (l = !0));
+    e(document).ready(function () {
+      var o = e(".hamburger"),
+        s = e(".overlay"),
+        i = !1;
+      o.click(function () {
+        1 == i
+          ? (s.hide(),
+            o.removeClass("is-open"),
+            o.addClass("is-closed"),
+            (i = !1))
+          : (s.show(),
+            o.removeClass("is-closed"),
+            o.addClass("is-open"),
+            (i = !0));
       }),
-        s('[data-toggle="offcanvas"]').click(function () {
-          s("#wrapper").toggleClass("toggled");
+        e('[data-toggle="offcanvas"]').click(function () {
+          e("#wrapper").toggleClass("toggled");
         });
     }),
     $(".slicknav_nav > li:last-child").after(
       '<li><a href="become-our-client.html" class="btn header-btn">Become Our Client <i class="ti-angle-right"></i></a></li>'
     );
-  var o = s(".h1-testimonial-active");
-  o.length &&
-    o.slick({
+  let s = document.querySelectorAll("section"),
+    i = document.querySelectorAll("#navigation li");
+  function l(e) {
+    i.forEach((e) => e.classList.remove("active")), e.classList.add("active");
+  }
+  i.forEach((e) =>
+    e.addEventListener("click", function () {
+      l(this);
+    })
+  ),
+    (window.onscroll = () => {
+      s.forEach((e) => {
+        let o = window.scrollY,
+          s = e.offsetTop,
+          i = e.offsetHeight,
+          t = e.getAttribute("id");
+        if (o >= s && o < s + i) {
+          l(document.querySelector(`[href='#${t}']`).parentElement);
+        }
+      });
+    });
+  var t = e(".h1-testimonial-active");
+  t.length &&
+    t.slick({
       dots: !1,
       infinite: !0,
       speed: 1e3,
@@ -68,9 +89,9 @@
         },
       ],
     });
-  var l = s(".completed-active");
-  l.length &&
-    l.owlCarousel({
+  var a = e(".completed-active");
+  a.length &&
+    a.owlCarousel({
       slidesToShow: 2,
       slidesToScroll: 1,
       loop: !0,
@@ -88,24 +109,24 @@
         1200: { items: 3 },
       },
     }),
-    s(window).on("scroll", function () {
-      s(window).scrollTop() < 500
-        ? s(".header-sticky").removeClass("sticky-bar")
-        : s(".header-sticky").addClass("sticky-bar");
+    e(window).on("scroll", function () {
+      e(window).scrollTop() < 500
+        ? e(".header-sticky").removeClass("sticky-bar")
+        : e(".header-sticky").addClass("sticky-bar");
     }),
-    s(window).on("scroll", function () {
-      s(window).scrollTop() < 500
-        ? s("#scrollUp").removeClass("show-top")
-        : s("#scrollUp").addClass("show-top");
+    e(window).on("scroll", function () {
+      e(window).scrollTop() < 500
+        ? e("#scrollUp").removeClass("show-top")
+        : e("#scrollUp").addClass("show-top");
     }),
-    s("[data-background]").each(function () {
-      s(this).css(
+    e("[data-background]").each(function () {
+      e(this).css(
         "background-image",
-        "url(" + s(this).attr("data-background") + ")"
+        "url(" + e(this).attr("data-background") + ")"
       );
     });
-  var i = s(".single_gallery_part, .img-pop-up");
-  i.length && i.magnificPopup({ type: "image", gallery: { enabled: !0 } }),
+  var n = e(".single_gallery_part, .img-pop-up");
+  n.length && n.magnificPopup({ type: "image", gallery: { enabled: !0 } }),
     $(".appr-h5").click(function () {
       $(".appr-desc.slide-down").toggleClass("slide-up slide-down"),
         $("i.ti-minus").toggleClass("ti-plus ti-minus"),
@@ -115,4 +136,10 @@
           .children(".appr-desc")
           .toggleClass("slide-up slide-down");
     });
+  var r = $(".fixme").offset().top;
+  $(window).scroll(function () {
+    $(window).scrollTop() >= r
+      ? $(".fixme").css({ position: "sticky", top: "100px" })
+      : $(".fixme").css({ position: "static" });
+  });
 })(jQuery);
